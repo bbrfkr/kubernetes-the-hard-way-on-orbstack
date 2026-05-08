3b4c5c07f6e62cc4ac0e9df47d1ca1a7ebe159c8
bbrfkr <bbrfkr@gmail.com>
2026-05-08 11:09:02 +0900

    update script

--- scripts/deploy-worker-node.sh
+++ scripts/deploy-worker-node.sh
@@ -7,9 +7,12 @@ apt-get -y install socat conntrack ipset kmod
 echo === 依存パッケージのインストール End ===
 
 # swapの無効化
+echo === swap無効化 Start ===
 swapoff -a
+echo === swap無効化 End ===
 
 # kernelパラメータの調整
+echo === kernelパラメータの調整 Start ===
 {
   modprobe br-netfilter
   echo "br-netfilter" >> /etc/modules-load.d/modules.conf
@@ -21,10 +24,13 @@ swapoff -a
     >> /etc/sysctl.d/kubernetes.conf
   sysctl -p /etc/sysctl.d/kubernetes.conf
 }
+echo === kernelパラメータの調整 End ===
 
 # systemdサービスの有効化
+echo === systemdサービスの有効化 Start ===
 {
   systemctl daemon-reload
   systemctl enable containerd kubelet kube-proxy
   systemctl restart containerd kubelet kube-proxy
 }
+echo === systemdサービスの有効化 End ===
